c766305a9645ad947a31ad1bb9b73132ac28a98c
Nazario Pizzicoli <nazario.pizzicoli@stud.unifi.it>
2026-01-30 15:07:49 +0100

    Update spawn_blue_cube.py

--- scripts/spawn_blue_cube.py
+++ scripts/spawn_blue_cube.py
@@ -70,9 +70,9 @@ def spawn_cube():
                                robot_namespace="/",
                                initial_pose=cube_pose,
                                reference_frame="world")
-        rospy.loginfo("Cubetto rosso spawnato in Gazebo ✅")
+        rospy.loginfo("Red cube spawned")
     except rospy.ServiceException as e:
-        rospy.logerr("Errore spawn cubo: %s", str(e))
+        rospy.logerr("Error spawn cube: %s", str(e))
 
 if __name__ == "__main__":
     spawn_cube()
